2a637407d2543f6b4ada8f4d7ee9cbf91e1a913d
AlexKnauth <alexander@knauth.org>
2016-05-13 20:44:41 -0400

    add dependency on "reprovide-lang"

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection 'multi)
 
-(define deps '("base" "generic-bind" "sweet-exp" "unstable-list-lib"))
+(define deps '("base" "generic-bind" "sweet-exp" "reprovide-lang" "unstable-list-lib"))
 
 (define build-deps '("rackunit" "scribble-lib" "scribble-code-examples" "racket-doc"))
 
