1c46aae00ec4b4d2b0c94330027112dd3dc72b86
AlexKnauth <alexander@knauth.org>
2016-05-16 20:50:00 -0400

    add missing dependency on "unstable-lib"

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection 'multi)
 
-(define deps '("base" "generic-bind" "sweet-exp" "reprovide-lang" "unstable-list-lib"))
+(define deps '("base" "generic-bind" "sweet-exp" "reprovide-lang" "unstable-lib" "unstable-list-lib"))
 
 (define build-deps '("rackunit" "scribble-lib" "scribble-code-examples" "racket-doc"))
 
